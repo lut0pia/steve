76341f055a1e91e4d1bc658997d95f1e9fa92d31
Lucien Catonnet <lutopia@hotmail.fr>
2016-12-17 17:59:45 +0100

    Added premake build system

---
+++ premake5.lua
@@ -0,0 +1,25 @@
+#!lua
+solution "Steve"
+	configurations {"Debug", "Release"}
+
+	project "Steve"
+		location("prj/".._ACTION)
+		language "C++"
+		files {"**.cpp","**.h"}
+		targetdir "bin"
+
+		configuration {"windows"}
+			links {"winmm"}
+
+		configuration {"Debug"}
+			targetname "Steve_dbg"
+			kind "ConsoleApp"
+			objdir("obj/".._ACTION.."/dbg")
+			defines {"L_DEBUG"}
+			symbols "On"
+
+		configuration {"Release"}
+			targetname "Steve"
+			kind "ConsoleApp"
+			objdir("obj/".._ACTION.."/rls")
+			flags {"Optimize"}
