06c0550717407661a8d3f704cfddb18516651301
Lucien Catonnet <lutopia@hotmail.fr>
2018-11-30 00:17:36 +0100

    pow in Creator::time is unnecessary

--- src/creator/Creator.cpp
+++ src/creator/Creator.cpp
@@ -1,13 +1,11 @@
 #include "Creator.h"
 
-#include <algorithm>
-
 #include "../Rand.h"
 
 using namespace steve;
 
 size_t Creator::time(size_t i, size_t size) const {
-  size_t wtr(pow(2, Rand::next(_mintime, _maxtime))); // 2^n ticks
+  size_t wtr(1 << Rand::next(_mintime, _maxtime)); // 2^n ticks
   while(i%wtr!=0 || i+wtr>size) // Good size and not too long
     wtr /= 2;
   return wtr;
