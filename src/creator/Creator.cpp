3d38d899ac06cebb74ffc5bea7d27484b6a6f75f
Lucien Catonnet <lutopia@hotmail.fr>
2018-12-15 22:55:04 +0100

    Formatting

--- src/creator/Creator.cpp
+++ src/creator/Creator.cpp
@@ -6,7 +6,7 @@ using namespace steve;
 
 size_t Creator::time(size_t i, size_t size) const {
   size_t wtr(1 << Rand::next(_mintime, _maxtime)); // 2^n ticks
-  while(i%wtr!=0 || i+wtr>size) // Good size and not too long
+  while(i%wtr != 0 || i + wtr > size) // Good size and not too long
     wtr /= 2;
   return wtr;
 }
