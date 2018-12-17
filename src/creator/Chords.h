7bb19bbb6e2448af13c472aa322c76e24168a714
Lucien Catonnet <lutopia@hotmail.fr>
2018-12-17 21:32:29 +0100

    Allow deferred start for chords

--- src/creator/Chords.h
+++ src/creator/Chords.h
@@ -8,7 +8,7 @@ namespace steve {
     uint32_t _md;
   public:
     inline Chords(const Music& m, int mintime = 0, int maxtime = 8)
-      : Creator(m, mintime, maxtime), _md(0) {}
+      : Creator(m, mintime, maxtime), _md(Rand::next(0, NoteValue::eighth)) {}
     Notes get(size_t start, size_t size) const override;
     const char* name() const override { return "Chords"; }
   };
