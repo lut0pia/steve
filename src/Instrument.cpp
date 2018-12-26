232af4cf57b58f89f93ca5e2259f937bee95e055
Lucien Catonnet <lutopia@hotmail.fr>
2018-12-26 19:56:31 +0100

    Removed obsolete Instrument functionality

--- src/Instrument.cpp
+++ src/Instrument.cpp
@@ -3,15 +3,3 @@
 using namespace steve;
 
 std::vector<Instrument> Instrument::_instruments;
-
-std::set<uint8_t> Instrument::in_range(const ToneSet& tones) const {
-  std::set<uint8_t> wtr;
-  for(uint8_t tone(0); tone<12; tone++) {
-    if(tones & (1<<tone)) {
-      for(uint8_t t(tone); t<=_max_tone; t += 12)
-        if(t>=_min_tone)
-          wtr.insert(t);
-    }
-  }
-  return wtr;
-}
--- src/Instrument.h
+++ src/Instrument.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <cstdint>
-#include <set>
 #include <string>
 #include <vector>
 
@@ -20,14 +19,10 @@ namespace steve {
       : _name(name), _midi_id(id), _min_tone(min_tone), _max_tone(max_tone) {
     }
 
-    // Return all tones corresponding to tones in the instrument's range
-    std::set<uint8_t> in_range(const ToneSet& tones) const;
-
     inline const std::string& name() const { return _name; }
     inline const uint8_t& midi_id() const { return _midi_id; }
     inline const uint8_t& min_tone() const { return _min_tone; }
     inline const uint8_t& max_tone() const { return _max_tone; }
-    inline uint8_t random_octave() const { return Rand::next(_min_tone/12, _max_tone/12)*12; }
     inline static void add(const Instrument& i) { _instruments.push_back(i); }
     inline static const Instrument* random() { return &Rand::in(_instruments); }
   };
