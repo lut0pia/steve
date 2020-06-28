44b4fbbcb63cccf73e4d506fa8c53ca515b19685
Lucien Catonnet <lutopia@hotmail.fr>
2020-06-28 16:48:44 +0200

    Added chort meta-events to MIDI files

--- src/Music.cpp
+++ src/Music.cpp
@@ -76,6 +76,18 @@ static void write_bigendian(std::ostream& s, uint32_t v, uint32_t byteCount) {
   if(byteCount>=2) s << uint8_t(v>>8);
   if(byteCount>=1) s << uint8_t(v);
 }
+class VarLength {
+  uint32_t _value;
+public:
+  inline VarLength(uint32_t value) : _value(value) {}
+  friend inline std::ostream& operator<<(std::ostream& s, const VarLength& v) {
+    if(v._value >= 128) {
+      s << (uint8_t)(((v._value >> 7) & 0x7f) | 0x80);
+    }
+    s << (uint8_t)(v._value & 0x7f);
+    return s;
+  }
+};
 void Music::write_mid(std::ostream& s) const {
   // Header chunk
   s << "MThd"; // Chunk id
@@ -90,16 +102,23 @@ void Music::write_mid(std::ostream& s) const {
   s << uint8_t(0); // Track event delta time
   s << uint8_t(0xFF) << uint8_t(0x51) << uint8_t(3); // Tempo meta event
   write_bigendian(s, 60000000u/_tempo, 3); // Microseconds per quarter note
-  for(uint32_t i(0); i<_creators.size(); i++) {
+  for(uint32_t i(0); i < _creators.size(); i++) {
     s << uint8_t(0) << uint8_t(0xC0|i) << _creators[i]->instrument()->midi_id(); // Program change
   }
-  uint32_t last(0);
-  for(auto&& note : _notes) {
-    s << uint8_t(note.first-last); // Track event delta time
+  uint32_t last = 0;
+  uint32_t last_chord = -1;
+  for(const auto& note : _notes) {
+    s << VarLength(note.first - last); // Track event delta time
     s << uint8_t((note.second.stop ? 0x80 : 0x90)|note.second.channel);
     s << uint8_t(note.second.tone);
     s << uint8_t(note.second.velocity);
     last = note.first;
+    if(note.first != last_chord && note.first % bar_ticks == 0) {
+      // Chord meta-event
+      std::string chord_str = chord_at(note.first).to_short_string();
+      s << uint8_t(0) << uint8_t(0xFF) << uint8_t(0x01) << VarLength(chord_str.size()) << chord_str; // Program change
+      last_chord = note.first;
+    }
   }
   s << uint8_t(0) << uint8_t(0xFF) << uint8_t(0x2F) << uint8_t(0); // End of track
   // Write chunk size
