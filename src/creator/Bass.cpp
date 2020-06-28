2ba64e1e4c35df7a3fc23af74a6a3023329863c8
Lucien Catonnet <lutopia@hotmail.fr>
2020-06-28 16:51:36 +0200

    Using root of chords in bass

--- src/creator/Bass.cpp
+++ src/creator/Bass.cpp
@@ -3,14 +3,21 @@
 using namespace steve;
 
 Bass::Bass(Music* music) : Creator(music) {}
+void Bass::init() {
+  Creator::init();
+
+  _min_tone = 36;
+  _max_tone = _min_tone + 12;
+
+  _min_time = _max_time = NoteValue(Rand::next(quarter, whole));
+}
 Notes Bass::get(size_t start, size_t size) const {
   Notes notes;
-  uintptr_t i(0);
+  uintptr_t i = 0;
   while(i < size) {
-    const size_t d(bar_ticks);
-    std::set<uint8_t> tones(choose_note_from_chord(_music->tones_at(start + i, d)));
-
-    uint8_t tone(Rand::in(tones));
+    const size_t d = 1 << _min_time;
+    const Chord& chord = _music->chord_at(start + i);
+    const uint8_t tone = _min_tone + chord.key();
     add_note(notes, _channel, tone, i, d);
 
     i += d;
@@ -18,7 +25,7 @@ Notes Bass::get(size_t start, size_t size) const {
   return notes;
 }
 bool Bass::is_valid_instrument(const Instrument& instrument) const {
-  return instrument.min_tone() <= 35;
+  return instrument.min_tone() <= 36 && instrument.max_tone() >= 48;
 }
 std::set<uint8_t> Bass::choose_note_from_chord(const ToneSet& tones) const {
   std::set<uint8_t> notes_in_ambitus;
--- src/creator/Bass.h
+++ src/creator/Bass.h
@@ -6,6 +6,7 @@ namespace steve {
   class Bass : public Creator {
   public:
     Bass(Music*);
+    virtual void init() override;
     virtual Notes get(size_t start, size_t size) const override;
     virtual const char* name() const override { return "Bass"; }
     virtual bool is_valid_instrument(const Instrument& instrument) const override;
