3113a98f9d8f3156bf1c07d929052cee08c8c0ee
Lucien Catonnet <lutopia@hotmail.fr>
2020-07-11 21:12:56 +0200

    Final tones size only have to be inferior to music tones

--- src/Music.cpp
+++ src/Music.cpp
@@ -123,7 +123,7 @@ std::string Music::to_short_string() const {
 }
 void Music::check() const {
   Tones final_tones(octave_tones(_notes));
-  assert(final_tones.size() == _tones.size());
+  assert(final_tones.size() <= _tones.size());
   for(uintptr_t i(0); i < final_tones.size(); i++) {
     assert((final_tones[i] | _scale.tones()) == _scale.tones());
     assert((final_tones[i] | _tones[i]) == _tones[i]);
