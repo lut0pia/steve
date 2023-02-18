89289124b9fc1a25e4e77d5a14b5bfb29e2e676a
Lutopia <lutopia@hotmail.fr>
2023-02-18 20:44:26 +0100

    Updated Chords to follow beats and use polyphonic instruments

--- src/creator/Chords.h
+++ src/creator/Chords.h
@@ -8,5 +8,6 @@ namespace steve {
     Chords(Music*);
     Notes get(size_t start, size_t size) const override;
     const char* name() const override { return "Chords"; }
+    bool is_valid_instrument(const struct Instrument& instrument) const override;
   };
 }
