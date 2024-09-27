e9a0ed09f421b133f275ed46163dbd99db231cfb
Lutopia <lutopia@hotmail.fr>
2024-09-27 21:40:15 +0200

    Default step for ranged numbers is one

--- src/cfg/ConfigRangedNumber.h
+++ src/cfg/ConfigRangedNumber.h
@@ -6,7 +6,7 @@ namespace steve {
   struct ConfigRangedNumber {
     float min = 0.f;
     float max = 1.f;
-    float step = 0.01f;
+    float step = 1.f;
     float sigma = 1.f;
 
     float get_value() const;
